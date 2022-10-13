13ddfe744f1a930e05445fa102fe450e5ebb81b1
Chadli <47243616+hchadli@users.noreply.github.com>
2022-10-13 14:08:45 +0200

    CosmosDB

---
+++ Backend/IRFestival.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using IRFestival.Api.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace IRFestival.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        public CosmosClient CosmosClient { get; set; }
+        public Container Container { get; set; }
+
+
+        public ArticlesController(IConfiguration configuration)
+        {
+            CosmosClient = new CosmosClient(configuration.GetConnectionString("CosmosConnection"));
+            Container = CosmosClient.GetContainer("IRFestivalArticles", "WebsiteArticles");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateItemAsync([FromBody] Article article)
+        {
+            await Container.CreateItemAsync(article);
+            return Ok();
+        }
+
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(Article))]
+        public async Task<IActionResult> GetAsync()
+        {
+
+            var database = await CosmosClient.CreateDatabaseIfNotExistsAsync("IRFestivalArticles");
+            var containerResponse = await database.Database.CreateContainerIfNotExistsAsync("WebsiteArticles", "/tag");
+            var container = containerResponse.Container;
+            
+            var result = new List<Article>();
+
+            var queryDefinition = container.GetItemLinqQueryable<Article>()
+                .Where(p => p.Status == nameof(Status.Published))
+                .OrderBy(p => p.Date);
+
+            var iterator = queryDefinition.ToFeedIterator();
+
+            while (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync();
+                result = response.ToList();
+            }
+
+            return Ok(result);
+        }
+
+
+    }
+}
--- Backend/IRFestival.Api/Domain/Article.cs
+++ Backend/IRFestival.Api/Domain/Article.cs
@@ -1,21 +1,35 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System;
 
 namespace IRFestival.Api.Domain
 {
     public class Article
     {
-        public string Id { get; set; }
 
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "tag")]
         public string Tag { get; set; }
 
+        [JsonProperty(PropertyName = "title")]
         public string Title { get; set; }
 
+
+        [JsonProperty(PropertyName = "subtitle")]
+        public string Subtitle { get; set; }        
+
+        [JsonProperty(PropertyName = "message")]
         public string Message { get; set; }
 
+
+        [JsonProperty(PropertyName = "imagePath")]
         public string ImagePath { get; set; }
 
+        [JsonProperty(PropertyName = "status")]
         public string Status { get; set; }
 
+        [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
     }
 
